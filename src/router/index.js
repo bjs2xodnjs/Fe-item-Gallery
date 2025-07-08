1b29663a414373383813da7ba24340c8f136d71b
taewon1918 <taewon1918@gmail.com>
2025-07-08 18:04:17 +0900

    [docs] home.vue 작업 및 로그인, 로그아웃 구현

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/join',
+      name: 'join',
+      component: () => import('@/views/join.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue')
+    }
+    
+  ],
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { getItems } from '@/services/itemService'
+import { onMounted } from 'vue';
+
+/*
+이 화면이 마운트될 때 딱 한번 getItems 통신하시고
+통신으로 받아온 데이터를 콘솔에 뿌려주세요.
+
+*/
+onMounted(async() => {
+  const res = await getItems();
+  console.log('res.data:', res.data);
+})
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="mt-5">HELLO, HOME</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
